24209dad5ca14bb5d2021021d58add7922211437
Clare <48302356+Neurogeneration@users.noreply.github.com>
2019-08-05 10:49:02 +0930

    Update Mutect2_Create_PONs.sh

--- Mutect2_Create_PONs.sh
+++ Mutect2_Create_PONs.sh
@@ -1,5 +1,21 @@
+#!/bin/sh
+#SBATCH -J CreatePONs
+#SBATCH -o /fast/users/a1092098/launch/slurm-%j.out
+#SBATCH -A robinson
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH --time=01:00:00
+#SBATCH --mem=8GB
 
+# Notification configuration
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=clare.vaneyk@adelaide.edu.au
 
+# load modules
+module load Java/1.8.0_121
+module load GATK
 
 CreateSomaticPanelOfNormals \
    -vcfs $FASTDIR/WGS/Mosaic/pon_vcf.list \
